184ddff4977d0d8fd1e93127aa7f02492eb46735
mina remon <minaremon20088@hotmail.com>
2020-08-16 15:54:32 +0200

    Make the heading with animation for the social items

--- js/app.js
+++ js/app.js
@@ -1,57 +1,15 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+/* Global Variables*/
+let facebook = document.getElementById('facebookicon');
+let twitter = document.getElementById('istgramicon');
+let instgram = document.getElementById('twittericon');
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+/* Event listener*/
+facebook.addEventListener('mouseover',()=> {
+     facebook.classList.toggle('animate__shakeX');
+});
+twitter.addEventListener('mouseover',()=>{
+     twitter.classList.toggle('animate__shakeX');
+})
+instgram.addEventListener('mouseover',()=>{
+      instgram.classList.toggle('animate__shakeX');
+})
